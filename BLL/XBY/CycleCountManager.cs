346877fff991f638c3477a34600ef61564c5ce8f
xby <1843903029@qq.com>
2020-04-16 17:35:14 +0800

    最后一次XBY

---
+++ BLL/XBY/CycleCountManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL;
+
+namespace BLL.XBY
+{
+   public class CycleCountManager
+    {
+        /// <summary>
+        /// 盘点分页查询
+        /// </summary>
+        /// <param name="PageIndex"></param>
+        /// <param name="PageSize"></param>
+        /// <param name="State"></param>
+        /// <returns></returns>
+        public static PageList PanDianList(int PageIndex, int PageSize, int State)
+        {
+            return DAL.XBY.CycleCountService.PanDianList(PageIndex, PageSize, State);
+        }
+        }
+}
