22e262221b5c8db3204b793cff315c1a61a9ac44
LLQ <3078628597@qq.com>
2020-04-16 16:15:01 +0800

    div

--- Storage/Controllers/LLQController.cs
+++ Storage/Controllers/LLQController.cs
@@ -17,23 +17,48 @@ namespace Storage.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 计量单位分页
+        /// </summary>
+        /// <param name="PageSize"></param>
+        /// <returns></returns>
+        public ActionResult Jldws(int PageSize)
+        {
+            return Json(BLL.LLQ.JldwManager.GetCount(PageSize), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetJldwAll(int PageIndex, int PageSize, Models.JLinfo jldw)
+        {
+            return Json(BLL.LLQ.JldwManager.Jldw(PageIndex, PageSize, jldw), JsonRequestBehavior.AllowGet);
+        }
+        
+
         public ActionResult Cplb()
         {
             return View();
         }
 
-        public ActionResult Indexs(int PageSize)
+
+        /// <summary>
+        /// 产品类别分页
+        /// </summary>
+        /// <param name="PageSize"></param>
+        /// <returns></returns>
+        public ActionResult Cplbs(int PageSize)
         {
             return Json(BLL.LLQ.CplbManager.GetCount(PageSize), JsonRequestBehavior.AllowGet);
         }
-        public ActionResult GetAll(int PageIndex, int PageSize, Models.CpLbinfo Cplb)
+        public ActionResult GetCplbAll(int PageIndex, int PageSize, Models.CpLbinfo Cplb)
         {
             return Json(BLL.LLQ.CplbManager.lb(PageIndex, PageSize, Cplb), JsonRequestBehavior.AllowGet);
         }
+
 
         public ActionResult CpGl()
         {
             return View();
         }
+
+
+
     }
 }
