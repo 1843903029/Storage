968130de7613c36914509471b9fd476fea9eb1e5
Administrator <Administrator@OS-20200101ZZBL>
2020-04-17 09:46:50 +0800

    4.17LL

---
+++ BLL/LL/KweiGuanliManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL;
+namespace BLL.LL
+{
+    public class KweiGuanliManager
+    {
+        public static PageList Listfenye(int pageindex, int pagesize)
+        {
+            return DAL.LL.KweiGuanli.Listfenye(pageindex, pagesize);
+        }
+    }
+}
---
+++ DAL/LL/KweiGuanli.cs
@@ -0,0 +1,30 @@
+﻿//using Microsoft.Analytics.Interfaces;
+//using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Models;
+
+namespace DAL.LL
+{
+    public class KweiGuanli
+    {
+        public static PageList Listfenye(int pageindex,int pagesize)
+        {
+            StorageEntities ent = new StorageEntities();
+            var obj = from p in ent.LocationManagement
+                      where p.Shuju == true
+                      orderby p.kwID
+                      select new
+                      {
+                          kwID = p.kwID
+                      };
+            PageList list = new PageList();
+            list.DataList = obj.Skip((pageindex - 1) * pagesize).Take(pagesize);
+            list.PageCount = obj.Count();
+            return list;
+        }
+    }
+}
