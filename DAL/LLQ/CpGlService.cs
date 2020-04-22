a95747a3a43b55158198de7762a26cba21b48f00
xby <1843903029@qq.com>
2020-04-22 14:24:48 +0800

    llq

--- DAL/LLQ/CpGlService.cs
+++ DAL/LLQ/CpGlService.cs
@@ -34,7 +34,7 @@ namespace DAL.LLQ
                           remark = p.remark
                       };
             PageList list = new PageList();
-            list.DataList = obj;
+            list.DataList = obj.Skip((pageIndex-1)*pageSize).Take(pageSize);
             list.PageCount = obj.Count();
 
             return list;
