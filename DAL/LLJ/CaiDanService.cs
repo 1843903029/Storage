4da3ba09f486f1823cf54e3961ce3aa82c44c85f
liaolongjiao <2910698163@qq.com>
2020-04-23 13:31:41 +0800

    llj

--- DAL/LLJ/CaiDanService.cs
+++ DAL/LLJ/CaiDanService.cs
@@ -6,8 +6,55 @@ using System.Threading.Tasks;
 using Models;
 
 namespace DAL.LLJ
+
 {
    public class CaiDanService
     {
+        public static PageList caidan(int pageIndex, int pageSize)
+        {
+            StorageEntities ent = new StorageEntities();
+            var obj = from p in ent.SysDepart
+                      where p.IsDelete == true
+                      orderby p.SysDepartID ascending
+                      select new
+                      {
+                          ID= p.SysDepartID,
+                          DepartNum = p.DepartNum,
+                          DepartName = p.DepartName,
+                        //  ChildCount = p.ChildCount,
+                        //  ParentNum = p.ParentNum,
+                       //   Depth = p.Depth,
+                          IsDelete=p.IsDelete,
+                          CreateTime=p.CreateTime,
+
+                      };
+            PageList list = new PageList();
+            list.DataList = obj;
+            list.PageCount = obj.Count();
+
+            //list.DataList = obj.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            ////设置总页数
+            //int rows = ent.SysRole.Count();
+            //list.PageCount = rows % pageSize == 0 ? rows / pageSize : rows / pageSize + 1;
+
+            return list;
+        }
+        //删除
+        public static int caidandel(SysDepart cd)
+        {
+            StorageEntities entity = new StorageEntities();
+            var obj = (from p in entity.SysDepart where p.SysDepartID == cd.SysDepartID select p).First();
+            obj.IsDelete = cd.IsDelete;
+            return entity.SaveChanges();
+
+        }
+        //新增
+        public static int caidanadd(SysDepart cd)
+        {
+            StorageEntities entity = new StorageEntities();
+            entity.SysDepart.Add(cd);
+            return entity.SaveChanges();
+        }
     }
 }
+
--- Storage/Controllers/LLJController.cs
+++ Storage/Controllers/LLJController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
+using BLL;
 
 namespace Storage.Controllers
 {
@@ -13,6 +15,30 @@ namespace Storage.Controllers
         {
             return View();
         }
+        //SysDepart
 
+        public ActionResult GetAllSysRole(int PageIndex, int PageSize)
+        {
+
+            return Json(BLL.LLJ.CanDanManager.SysRolefenye(PageIndex, PageSize), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult caidandel(SysDepart cd)
+        {
+
+            return Json(BLL.LLJ.CanDanManager.caidandel(cd), JsonRequestBehavior.AllowGet);
+        }
+        //新增
+        public ActionResult caidanadd(SysDepart cd)
+        {
+
+            return Json(BLL.LLJ.CanDanManager.caidanadd(cd), JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult SysDepart()
+        {
+            return View();
+        }
+      
     }
 }
