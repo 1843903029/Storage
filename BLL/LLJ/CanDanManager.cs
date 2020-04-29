6b2c86129581e6f7995c4a597cb282dde0fbbc68
liaolongjiao <2910698163@qq.com>
2020-04-29 19:11:23 +0800

    LLJ

--- BLL/LLJ/CanDanManager.cs
+++ BLL/LLJ/CanDanManager.cs
@@ -10,18 +10,33 @@ namespace BLL.LLJ
 {
    public class CanDanManager
     {
-        public static Models.PageList SysRolefenye(int pageIndex, int pageSize)
+        public static Models.PageList Functionfenye(int pageIndex, int pageSize)
         {
 
             return DAL.LLJ.CaiDanService.caidan(pageIndex, pageSize);
         }
-        public static int caidandel(int SysDepartID)
+        public static int caidandel(int NodeId)
         {
-            return DAL.LLJ.CaiDanService.caidandel(SysDepartID);
+            return DAL.LLJ.CaiDanService.caidandel(NodeId);
         }
-        public static int caidanadd(SysDepart cd)
+        public static int caidanadd(Function cd)
         {
             return DAL.LLJ.CaiDanService.caidanadd(cd);
+        }
+        public static IQueryable FunctionGetById(int NodeId)
+        {
+            return DAL.LLJ.CaiDanService.FunctionGetById(NodeId);
+        }
+
+        public static int FunctionEdit(int NodeId, string DisplayName)
+        {
+            return DAL.LLJ.CaiDanService.FunctionEdit( NodeId, DisplayName);
+
+        }
+        //模糊查询
+        public static IQueryable FunctionQuery(int pageIndex, int pageSize, string DisplayName)
+        {
+            return DAL.LLJ.CaiDanService.FunctionQuery(pageIndex, pageSize, DisplayName);
         }
     }
 }
