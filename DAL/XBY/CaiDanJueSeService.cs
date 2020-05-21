3268b2c03997c2b1f638953ad0f825d905d0bc36
xby <1843903029@qq.com>
2020-05-21 14:30:54 +0800

    5.21_下午

---
+++ DAL/XBY/CaiDanJueSeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DAL.XBY
+{
+   public class CaiDanJueSeService
+    {
+
+        //public static IQueryable CaiDanList()
+        //{
+        //    StorageEntities ent = new StorageEntities();
+        //    var obj=from p in ent.Function
+        //}
+    }
+}
